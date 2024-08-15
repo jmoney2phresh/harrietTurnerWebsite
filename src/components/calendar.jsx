88d3887fb8947ef2828d2b095aba044ea216c13b
jmoney2phresh <jackwbsymonds@gmail.com>
2024-08-15 10:50:04 -0400

    updated media queries

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -11,10 +11,10 @@ const Calendar = () => {
         <iframe
           src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ25Am5Zz3tNMWYR7rcWGSJygFE2vlkALhKmVJ1_B7MsteA2dixDwtlsQQbaYgtYxpllQwsoKWqb?gv=true"
           style={{ border: 0 }}
-          width="100%"
-          height="600"
+          width="80%"
+          padding="10%"
+          height="700"
           frameborder="0"
-          title="Google Calendar Appointment Scheduling"
         ></iframe>
       </div>
       <Footer />
